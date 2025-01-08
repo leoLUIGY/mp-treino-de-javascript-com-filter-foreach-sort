6a6d1a6f6648fd2d8cb62dbd1be96af359560568
Carol Silva <contatocarolinebs@gmail.com>
2025-01-08 19:49:06 -0300

    added data.js

---
+++ data.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    name: "iPhone 16",
+    price: 1899,
+    originalPrice: 2199.90,
+    paymentMethod: "PIX",
+    rating: 5,
+    reviews: 6,
+    description: "O novíssimo sistema de câmera é muito versátil de perto ou de longe. Câmera de 48 MP",
+    srcImg: "./assets/iphone16.png",
+    backImg: "./assets/backIphone.png"
+  },
+  {
+    name: "Samsung Z",
+    price: 2899,
+    originalPrice: 2199.90,
+    paymentMethod: "PIX",
+    rating: 3,
+    reviews: 3,
+    description: "O novíssimo sistema de câmera é muito versátil de perto ou de longe. Câmera de 48 MP",
+    srcImg: "./assets/celulares/samsungfrente.png",
+    backImg: "./assets/celulares/samsungback.png"
+  },
+  {
+    name: "Xiaomi Mi 9",
+    price: 3899,
+    originalPrice: 2199.90,
+    paymentMethod: "PIX",
+    rating: 2,
+    reviews: 6,
+    description: "O novíssimo sistema de câmera é muito versátil de perto ou de longe. Câmera de 48 MP",
+    srcImg: "./assets/celulares/xiaomifrente.png",
+    backImg: "./assets/celulares/xiaomiback.png"
+
+  },
+  {
+    name: "Poco x3 ",
+    price: 3899,
+    originalPrice: 2199.90,
+    paymentMethod: "PIX",
+    rating: 5,
+    reviews: 6,
+    description: "O novíssimo sistema de câmera é muito versátil de perto ou de longe. Câmera de 48 MP",
+    srcImg: "./assets/celulares/pocofrente.png",
+    backImg: "./assets/celulares/pocoback.png"
+
+  },
+ 
+]
+
